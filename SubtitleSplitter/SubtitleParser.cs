c2bfd1df67cc8482d67dd4ebb84ddb78d3dac357
Lakhwinder <lakhwinder_gill@hotmail.com>
2018-03-14 12:35:26 +1100

    Add project files.

---
+++ SubtitleSplitter/SubtitleParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SubtitleSplitter
+{
+    public class SubtitleParser
+    {
+        public string[] Parse(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
